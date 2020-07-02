99bfd85a8eb03769dae6e125a5c373da8d849ca2
Pitwuk <45837428+Pitwuk@users.noreply.github.com>
2020-07-02 14:34:29 -0400

    restart

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <div id="app">
-    <Homepage />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Homepage from "./components/Homepage.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
+  name: 'App',
   components: {
-    Homepage
+    HelloWorld
   }
-};
+}
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #d1c1b4;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
