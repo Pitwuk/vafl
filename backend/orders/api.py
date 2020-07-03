e79e91d4a408f5558f0785f3e687224be95c7978
Pitwuk <45837428+Pitwuk@users.noreply.github.com>
2020-07-03 19:39:23 -0400

    setup django

---
+++ backend/orders/api.py
@@ -0,0 +1,13 @@
+from .models import Order
+from rest_framework import viewsets, permissions
+from .serializers import OrderSerializer
+
+# Order Viewset
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = OrderSerializer
---
+++ backend/orders/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Order(models.Model):
+    orderNum = models.CharField(max_length=16, unique=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    first = models.CharField(max_length=128, default='')
+    last = models.CharField(max_length=128, default='')
+    email = models.EmailField(max_length=128, default='')
+    address = models.CharField(max_length=128, default='')
+    city = models.CharField(max_length=128, default='')
+    state = models.CharField(max_length=2, default='')
+    zipCode = models.IntegerField(default=00000)
+    quantity = models.IntegerField(default=1)
+    speed = models.CharField(max_length=8, default='')
+    color = models.CharField(max_length=5, default='')
+    layers = models.IntegerField(default=2)
+    gerber = models.FileField(
+        upload_to='orders/gerbers/')
