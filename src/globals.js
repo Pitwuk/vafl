170a8c185201169a1cb2c7e141e457074827dbe1
Pitwuk <45837428+Pitwuk@users.noreply.github.com>
2020-09-23 11:41:29 -0400

    add cart

---
+++ src/globals.js
@@ -0,0 +1,24 @@
+//Board options
+var width = 0;
+var height = 0;
+var quantity = 1;
+var speed = "Economy";
+var color = "Any";
+var layers = "2";
+var request = "";
+
+var cart = ["hee", "hee"];
+
+//Personal Info
+var firstName = "";
+var lastName = "";
+var email = "";
+var address = "";
+var city = "";
+var state = "";
+var zip = 0;
+
+//order info
+var orderNum = "";
+var price = 0;
+var shippingPrice = 0;
